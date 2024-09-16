73c7c283ca78cc7e7f9f1da472d1b611a34c3577
Sam El-Kamand <sam.elkamand@gmail.com>
2024-09-16 16:02:23 +1000

    feat: added horizontal argument to coord_flip bootstraps

--- R/sig_visualise_bootstraps.R
+++ R/sig_visualise_bootstraps.R
@@ -6,6 +6,9 @@
 #' @param bootstraps A dataframe in sigverse style describing bootstraps. See [sigshared::example_bootstraps()].
 #' @param min_contribution_threshold Numeric value representing the minimum contribution threshold to consider (default is 0.05). See threshold argument of [sigstats::sig_compute_experimental_p_value()].
 #' @param pvalue The p-value threshold for significance (default is 0.05). P values are computed using [sigstats::sig_compute_experimental_p_value()]
+#' @param horizontal flip the coordinates so that signatures are on the X axis and contributions are on the Y axis
+#' @inheritDotParams boxplotstats::plot_boxplot_stats
+#'
 #' @return A ggplot object representing the boxplot visualization of bootstrap contributions.
 #' @export
 #'
@@ -13,7 +16,9 @@
 #' library(sigshared)
 #' sig_visualise_bootstraps(example_bootstraps())
 #'
-sig_visualise_bootstraps <- function(bootstraps, min_contribution_threshold = 0.05, pvalue = 0.05) {
+sig_visualise_bootstraps <- function(
+    bootstraps, min_contribution_threshold = 0.05, pvalue = 0.05, horizontal = FALSE, ...
+    ) {
   # Assert that the bootstraps dataframe is in the correct format
   sigshared::assert_bootstraps(bootstraps)
 
@@ -36,12 +41,13 @@ sig_visualise_bootstraps <- function(bootstraps, min_contribution_threshold = 0.
     col_data_id = "signatures",
     show_legend = FALSE,
     sort = TRUE,
-    descending = TRUE
+    descending = if(horizontal) FALSE else TRUE,
+    ...
   ) +
     # Add vertical grid lines
     ggplot2::theme(panel.grid.major.x = ggplot2::element_line(linetype = "longdash", colour = "lightgrey")) +
     # Add vertical line at the minimum contribution threshold
-    ggplot2::geom_vline(xintercept = min_contribution_threshold, linetype = "longdash", colour = "red") +
+    ggplot2::geom_vline(xintercept = min_contribution_threshold, linetype = "dashed", colour = "red") +
     # Format x-axis labels as percentages
     ggplot2::scale_x_continuous(labels = scales::label_percent()) +
     # Define fill colors for significant and non-significant contributions
@@ -49,5 +55,16 @@ sig_visualise_bootstraps <- function(bootstraps, min_contribution_threshold = 0.
     # Define outline colors for significant and non-significant contributions
     ggplot2::scale_colour_manual(values = c("FALSE" = "grey30", "TRUE" = "#077969"))
 
+
+  if(horizontal){
+    plot <- plot +
+      ggplot2::coord_flip() +
+      ggplot2::theme(
+        axis.text.x = ggplot2::element_text(angle = 90, vjust=0.5, hjust = 1),
+        panel.grid.major.y = ggplot2::element_line(linetype = "longdash", colour = "lightgrey"),
+        panel.grid.major.x = ggplot2::element_blank(),
+        panel.grid.minor.y = ggplot2::element_blank(),
+        )
+  }
   return(plot)
 }
