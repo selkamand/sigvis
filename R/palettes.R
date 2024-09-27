81e783060fd22d248fca92fef3275bce96a5e2eb
Sam El-Kamand <sam.elkamand@gmail.com>
2024-09-27 15:39:01 +1000

    feat: added auto coloring and sorting for RNA SBS
    solves issue #37

--- R/palettes.R
+++ R/palettes.R
@@ -109,6 +109,25 @@ sig_palette_sv_type <- function(){
   )
 }
 
+#' @inherit sig_palette_snv_type title description details seealso sections references examples
+#' @export
+sig_palette_rnasnv_type <- function(){
+  c(
+    "C>A" = "#20BDF3",
+    "C>G" = "#060709",
+    "C>T" = "#E72826",
+    "T>A" = "#CACACA",
+    "T>C" = "#A5CE61",
+    "T>G" = "#EEC8C5",
+    "G>T" = "#98D4EA",
+    "G>C" = "#5D6164",
+    "G>A" = "#E06D15",
+    "A>T" = "#EAE8EB",
+    "A>G" = "#DBF578",
+    "A>C" = "#F7DADB"
+  )
+}
+
 #' The set2 colour palette
 #'
 #' @return the set2 colour paletted as a named vector
@@ -151,7 +170,8 @@ auto_palette <- function(types, default = pal_set2()){
     dbs_type = sig_palette_doublet_type(),
     cn_type = sig_palette_cn_type(),
     cn2_type = sig_palette_cn2_type(),
-    sv_type = sig_palette_sv_type()
+    sv_type = sig_palette_sv_type(),
+    rnasnv_type = sig_palette_rnasnv_type()
     )
 
   n_matches = vapply(ls_pals, \(pal){ sum(types %in% names(pal)) }, FUN.VALUE = numeric(1))
@@ -313,7 +333,74 @@ levels_sv_type <- function(){
   )
 }
 
+levels_rnasnv <- function(){
+  c(
+    "A[C>A]A", "A[C>A]C", "A[C>A]G", "A[C>A]T",
+    "C[C>A]A", "C[C>A]C", "C[C>A]G", "C[C>A]T",
+    "G[C>A]A", "G[C>A]C", "G[C>A]G", "G[C>A]T",
+    "T[C>A]A", "T[C>A]C", "T[C>A]G", "T[C>A]T",
 
+    "A[C>G]A", "A[C>G]C", "A[C>G]G", "A[C>G]T",
+    "C[C>G]A", "C[C>G]C", "C[C>G]G", "C[C>G]T",
+    "G[C>G]A", "G[C>G]C", "G[C>G]G", "G[C>G]T",
+    "T[C>G]A", "T[C>G]C", "T[C>G]G", "T[C>G]T",
+
+    "A[C>T]A", "A[C>T]C", "A[C>T]G", "A[C>T]T",
+    "C[C>T]A", "C[C>T]C", "C[C>T]G", "C[C>T]T",
+    "G[C>T]A", "G[C>T]C", "G[C>T]G", "G[C>T]T",
+    "T[C>T]A", "T[C>T]C", "T[C>T]G", "T[C>T]T",
+
+    "A[T>A]A", "A[T>A]C", "A[T>A]G", "A[T>A]T",
+    "C[T>A]A", "C[T>A]C", "C[T>A]G", "C[T>A]T",
+    "G[T>A]A", "G[T>A]C", "G[T>A]G", "G[T>A]T",
+    "T[T>A]A", "T[T>A]C", "T[T>A]G", "T[T>A]T",
+
+    "A[T>C]A", "A[T>C]C", "A[T>C]G", "A[T>C]T",
+    "C[T>C]A", "C[T>C]C", "C[T>C]G", "C[T>C]T",
+    "G[T>C]A", "G[T>C]C", "G[T>C]G", "G[T>C]T",
+    "T[T>C]A", "T[T>C]C", "T[T>C]G", "T[T>C]T",
+
+    "A[T>G]A", "A[T>G]C", "A[T>G]G", "A[T>G]T",
+    "C[T>G]A", "C[T>G]C", "C[T>G]G", "C[T>G]T",
+    "G[T>G]A", "G[T>G]C", "G[T>G]G", "G[T>G]T",
+    "T[T>G]A", "T[T>G]C", "T[T>G]G", "T[T>G]T",
+
+    "A[G>T]A", "A[G>T]C", "A[G>T]G", "A[G>T]T",
+    "C[G>T]A", "C[G>T]C", "C[G>T]G", "C[G>T]T",
+    "G[G>T]A", "G[G>T]C", "G[G>T]G", "G[G>T]T",
+    "T[G>T]A", "T[G>T]C", "T[G>T]G", "T[G>T]T",
+
+    "A[G>C]A", "A[G>C]C", "A[G>C]G", "A[G>C]T",
+    "C[G>C]A", "C[G>C]C", "C[G>C]G", "C[G>C]T",
+    "G[G>C]A", "G[G>C]C", "G[G>C]G", "G[G>C]T",
+    "T[G>C]A", "T[G>C]C", "T[G>C]G", "T[G>C]T",
+
+    "A[G>A]A", "A[G>A]C", "A[G>A]G", "A[G>A]T",
+    "C[G>A]A", "C[G>A]C", "C[G>A]G", "C[G>A]T",
+    "G[G>A]A", "G[G>A]C", "G[G>A]G", "G[G>A]T",
+    "T[G>A]A", "T[G>A]C", "T[G>A]G", "T[G>A]T",
+
+    "A[A>T]A", "A[A>T]C", "A[A>T]G", "A[A>T]T",
+    "C[A>T]A", "C[A>T]C", "C[A>T]G", "C[A>T]T",
+    "G[A>T]A", "G[A>T]C", "G[A>T]G", "G[A>T]T",
+    "T[A>T]A", "T[A>T]C", "T[A>T]G", "T[A>T]T",
+
+    "A[A>G]A", "A[A>G]C", "A[A>G]G", "A[A>G]T",
+    "C[A>G]A", "C[A>G]C", "C[A>G]G", "C[A>G]T",
+    "G[A>G]A", "G[A>G]C", "G[A>G]G", "G[A>G]T",
+    "T[A>G]A", "T[A>G]C", "T[A>G]G", "T[A>G]T",
+
+    "A[A>C]A", "A[A>C]C", "A[A>C]G", "A[A>C]T",
+    "C[A>C]A", "C[A>C]C", "C[A>C]G", "C[A>C]T",
+    "G[A>C]A", "G[A>C]C", "G[A>C]G", "G[A>C]T",
+    "T[A>C]A", "T[A>C]C", "T[A>C]G", "T[A>C]T"
+  )
+}
+
+levels_rnasnv_type <- function(){
+  c("C>A", "C>G", "C>T", "T>A", "T>C", "T>G", "G>T", "G>C", "G>A",
+    "A>T", "A>G", "A>C")
+}
 
 
 # Autosort Levels -------------------------------------------------------
@@ -335,7 +422,9 @@ auto_level <- function(set, type = c('channel', 'type')) {
     cn_type = levels_cn_type(),
     cn2_type = levels_cn2_type(),
     sv_32 = levels_sv(),
-    sv_type = levels_sv_type()
+    sv_type = levels_sv_type(),
+    rnasnv = levels_rnasnv(),
+    rnasnv_type = levels_rnasnv_type()
   )
 
   # Check if unique set match any known channel/type set
